945791f4ed693324332449d1dbf600ab2281215c
Vaibhav <vaibhavg7005@gmail.com>
2023-06-13 13:09:34 +0530

    using view binding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.myapplication
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnDisplaySnackbar.setOnClickListener {
+            displaySnackbar()
+        }
+        binding.btnDisplayDialog.setOnClickListener {
+            displayDialog()
+        }
+    }
+
+    private fun displaySnackbar() {
+        Snackbar.make(binding.containerLayout, "Live freely", Snackbar.LENGTH_INDEFINITE)
+            .setAction("op"){}
+            .show()
+    }
+
+    private fun displayDialog() {
+        AlertDialog.Builder(this).setTitle("Alert").setPositiveButton("yes") { it, _ -> it.cancel() }
+            .show()
     }
 }
