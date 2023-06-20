220fd6eb619f52fdb79f7fdcaffd344dbd39fdb0
Vaibhav <vaibhavg7005@gmail.com>
2023-06-20 08:01:06 +0530

    Added basic lifecycle methods

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,12 +5,14 @@ import android.app.AlertDialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("vaibhav", "onCreateChild")
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -20,6 +22,21 @@ class MainActivity : AppCompatActivity() {
         binding.btnDisplayDialog.setOnClickListener {
             displayDialog()
         }
+    }
+
+    override fun onPause() {
+        Log.d("vaibhav", "onPauseChild")
+        super.onPause()
+    }
+
+    override fun onStart() {
+        Log.d("vaibhav", "onStartChild")
+        super.onStart()
+    }
+
+    override fun onStop() {
+        Log.d("vaibhav", "onStopChild")
+        super.onStop()
     }
 
     private fun displaySnackbar() {
