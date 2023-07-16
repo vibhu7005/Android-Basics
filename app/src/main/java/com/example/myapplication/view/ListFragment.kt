1aa8218737855e70603182c1daef0553285d18c4
vaibhav <vaibhavg7005@gmail.com>
2023-07-16 23:37:58 +0530

    grid view overview

--- app/src/main/java/com/example/myapplication/view/ListFragment.kt
+++ app/src/main/java/com/example/myapplication/view/ListFragment.kt
@@ -1,13 +1,19 @@
 package com.example.myapplication.view
 
+import android.graphics.drawable.GradientDrawable.Orientation
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.myapplication.databinding.FragmentListBinding
 import com.example.myapplication.view.adapter.CountryListAdapter
+import com.example.myapplication.view.adapter.CountryListGridAdapter
 
 class ListFragment : Fragment() {
     lateinit var binding: FragmentListBinding
@@ -30,14 +36,12 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val countryList = listOf("Bangkok", "London", "Tokyo")
-        val adapter = CountryListAdapter(countryList)
-        binding.rvListStudents.layoutManager = LinearLayoutManager(context)
-        binding.rvListStudents.adapter = adapter
-//        binding.listStudents.adapter = adapter
-//        binding.listStudents.setOnItemClickListener { _, _, position, _ ->
-//            Toast.makeText(context, countryList[position], Toast.LENGTH_SHORT).show()
-//        }
+        val countryList = listOf("Bangkok", "London", "Tokyo", "mapple", "Sydney", "Austin", "Russia", "Bolavia", "Libya", "Sudan", "Nigeria", "India", "Bhutan", "Nepal", "Australia", "Canada", "USA")
+        val adapter = CountryListAdapter(countryList, requireContext())
+        binding.rvListCountries.layoutManager = GridLayoutManager( context, 2 ,RecyclerView.VERTICAL, false)
+        binding.rvListCountries.adapter = adapter
+        binding.gvListStudents.adapter = CountryListGridAdapter(countryList)
+
     }
 
     override fun onResume() {
