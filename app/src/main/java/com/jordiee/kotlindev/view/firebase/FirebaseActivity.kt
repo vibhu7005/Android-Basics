b0e05eab67e4fb62f655a000d84dfc7c2e05ae32
vaibhav <vaibhavg7005@gmail.com>
2023-09-20 22:53:31 +0530

    Set up basic 2 way communication with real time database

--- app/src/main/java/com/jordiee/kotlindev/view/firebase/FirebaseActivity.kt
+++ app/src/main/java/com/jordiee/kotlindev/view/firebase/FirebaseActivity.kt
@@ -2,13 +2,18 @@ package com.jordiee.kotlindev.view.firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 
 import com.jordiee.kotlindev.databinding.ActivityFirebaseBinding
 
 class FirebaseActivity : AppCompatActivity() {
-    val database  = FirebaseDatabase.getInstance()
-    val references = database.reference.child("Users")
+    private val database  = FirebaseDatabase.getInstance()
+    private val usersReference = database.reference.child("Users")
+    private val databaseReference = database.reference
 
     private lateinit var binding: ActivityFirebaseBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +21,17 @@ class FirebaseActivity : AppCompatActivity() {
         binding = ActivityFirebaseBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btAdd.setOnClickListener {
-            references.child("Age").setValue(binding.etName.text.toString())
+            usersReference.child("Name").setValue(binding.etName.text.toString())
         }
+
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.etName.setText(snapshot.child("Users").child("Name").value.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
