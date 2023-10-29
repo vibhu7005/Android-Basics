4d258318225535eb25035ce60ecd755277f25cdf
vaibhav <vaibhavg7005@gmail.com>
2023-10-29 15:54:02 +0530

    Adding, updating , deleting data from firebase real time database

--- app/src/main/java/com/jordiee/kotlindev/view/firebase/FirebaseActivity.kt
+++ app/src/main/java/com/jordiee/kotlindev/view/firebase/FirebaseActivity.kt
@@ -15,24 +15,32 @@ import com.jordiee.kotlindev.databinding.ActivityFirebaseBinding
 import com.jordiee.kotlindev.view.firebase.data.Users
 
 class FirebaseActivity : AppCompatActivity() {
-    private val database  = FirebaseDatabase.getInstance()
+    private val database = FirebaseDatabase.getInstance()
     private val databaseReference = database.reference.child("Users")
 
     private lateinit var binding: ActivityFirebaseBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFirebaseBinding.inflate(layoutInflater)
-//        binding.rvUsers.layoutManager = LinearLayoutManager(this)
-//        binding.rvUsers.adapter =
         setContentView(binding.root)
-//        retrieveData()
         binding.btAdd.setOnClickListener { addUserToDatabase() }
+        updateData()
+        fetchUsersData()
+        removeData()
+    }
+
+    private fun updateData() {
+        val map: MutableMap<String?, Any?> = mutableMapOf()
+        map["email"] = "updatedTwice"
+        databaseReference.child("-NhumGZtUVSg4liF1Suk").updateChildren(map)
+    }
+
+    private fun removeData() {
+        databaseReference.child("-NhumGZtUVSg4liF1Suk").removeValue()
     }
     private fun addUserToDatabase() {
         val id = databaseReference.push().key.toString()
         val user = Users(id, binding.etName.text.toString(), binding.etEmail.text.toString())
-        databaseReference.child("monkey").setValue("loop")
-        databaseReference.child("monkey").setValue("lock")
        databaseReference.child(id).setValue(user).addOnCompleteListener {
            if (it.isSuccessful) {
                Toast.makeText(this, "added", Toast.LENGTH_SHORT).show()
@@ -40,6 +48,25 @@ class FirebaseActivity : AppCompatActivity() {
                Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
            }
        }
+    }
+
+   private fun fetchUsersData() {
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val users = snapshot.children
+                for (data in users) {
+                    val user = data.getValue(Users::class.java)
+                    println(user?.id)
+                    println(user?.name)
+                    println(user?.email)
+                    println()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
     private fun retrieveData() {
